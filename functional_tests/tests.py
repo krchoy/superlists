087203cc2911016ba900d9c61536c30893a05f2f
Choy <kerry.choy@hpe.com>
2016-01-22 12:27:37 +1100

    page 92 starting point for custom url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(LiveServerTestCase):
 		
 	def tearDown(self):
 		self.browser.close()
+		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.live_server_url)
@@ -39,8 +44,8 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 		
 # The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
-		self.check_for_row_in_list_table('1: Buy pea peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
 
 # Now a new user, Francis, comes to the site
@@ -61,7 +66,7 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 		
 #Francis has a unique url
-		francis_list_url = self.current_url
+		francis_list_url = self.browser.current_url
 		self.assertRegex(francis_list_url, '/lists/.+')
 		self.assertNotEqual(francis_list_url, edith_list_url)
 
