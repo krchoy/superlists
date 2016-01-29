3a5ce410f162d5837eaf4179594f3abf7abb2882
Kerry Choy <krchoy@gmail.com>
2016-01-29 22:32:38 +1100

    where am i?

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[i]
+				return
+			super().setUpClass()
+			cls.server_url = cls.live_server_url
+			
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+	
 	def setUp(self):
 		self.browser = webdriver.Chrome('../chromedriver.exe')
 		self.browser.implicitly_wait(3)
@@ -16,7 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
@@ -55,7 +71,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Chrome('../chromedriver.exe')
 		
 #Francis should not see Edith's list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -79,7 +95,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#Edith goes to homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 		
 		#She notices the input box is nicely centered
